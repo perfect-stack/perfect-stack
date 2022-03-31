6f3a13fe31be8e464024f13d27633d7f64225cb3
Richard Perfect <richard@june365.com>
2022-04-01 11:09:32 +1300

    demo-aws-server github actions

--- demo/aws/demo-aws-server/publish-docker-aws.sh
+++ demo/aws/demo-aws-server/publish-docker-aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 100150877581.dkr.ecr.ap-southeast-2.amazonaws.com
-docker build --platform linux/amd64 -t demo-aws-server .
+docker build --build-arg NPM_TOKEN=${NPM_TOKEN} --platform linux/amd64 -t demo-aws-server .
 
 export NPM_PKG_VERSION=$(npm pkg get version | sed 's/"//g')
 docker tag  demo-aws-server:latest 100150877581.dkr.ecr.ap-southeast-2.amazonaws.com/demo-aws-server:$NPM_PKG_VERSION
