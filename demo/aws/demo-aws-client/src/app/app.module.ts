bd345142181052bb4fa9b7c5bdec3c5e3de0a287
Richard Perfect <richard@june365.com>
2022-04-05 15:16:59 +1200

    setup for client.json

--- demo/aws/demo-aws-client/src/app/app.module.ts
+++ demo/aws/demo-aws-client/src/app/app.module.ts
@@ -1,21 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgxPerfectStackModule} from '@perfect-stack/ngx-perfect-stack';
-import {environment} from '../environments/environment';
-
+import { NgxPerfectStackModule} from '@perfect-stack/ngx-perfect-stack';
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NgxPerfectStackModule.forRoot(environment),
-    ],
+  imports: [
+      BrowserModule,
+      AppRoutingModule,
+      NgxPerfectStackModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- demo/aws/demo-aws-client/src/main.ts
+++ demo/aws/demo-aws-client/src/main.ts
@@ -1,12 +1,25 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {NgxPerfectStackConfig, STACK_CONFIG} from '@perfect-stack/ngx-perfect-stack';
+import {AppModule} from './app/app.module';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+fetch('/client.json')
+  .then( (configResponse) => configResponse.json())
+  .then( (configJson) => {
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    if(configJson.production) {
+      enableProdMode();
+    }
+
+    console.log('Downloaded configJson: ', configJson);
+    const stackConfig: NgxPerfectStackConfig = {
+      apiUrl: configJson.API_URL
+    };
+
+    platformBrowserDynamic([
+      { provide: STACK_CONFIG, useValue: stackConfig}
+    ])
+      .bootstrapModule(AppModule)
+      .catch(err => console.error(err))
+  });
