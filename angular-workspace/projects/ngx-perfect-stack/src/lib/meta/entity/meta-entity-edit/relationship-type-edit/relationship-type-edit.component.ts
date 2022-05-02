13a2b5591617615686c955492a2c1ef68af87138
Richard Perfect <richard@june365.com>
2022-05-02 12:32:15 +1200

    fixed bug with relational type

--- angular-workspace/projects/ngx-perfect-stack/src/lib/meta/entity/meta-entity-edit/relationship-type-edit/relationship-type-edit.component.ts
+++ angular-workspace/projects/ngx-perfect-stack/src/lib/meta/entity/meta-entity-edit/relationship-type-edit/relationship-type-edit.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
 import {AttributeType, MetaEntity} from '../../../../domain/meta.entity';
 import {MetaEntityService} from '../../meta-entity-service/meta-entity.service';
 import {FormGroup} from '@angular/forms';
@@ -14,16 +13,20 @@ export class RelationshipTypeEditComponent implements OnInit {
   @Input()
   formGroup: FormGroup;
 
-  metaEntityOptions$: Observable<MetaEntity[]>
+  @Input()
+  metaEntityOptions: MetaEntity[];
+
   targetMetaEntity: MetaEntity;
 
   constructor(protected readonly metaEntityService: MetaEntityService) { }
 
   ngOnInit(): void {
-    this.metaEntityOptions$ = this.metaEntityService.findAll();
+    //this.metaEntityOptions$ = this.metaEntityService.findAll();
 
     const relationshipTarget = this.formGroup?.controls['relationshipTarget']?.value;
-    this.metaEntityService.findById(relationshipTarget).subscribe(metaEntity => this.targetMetaEntity = metaEntity);
+    if(relationshipTarget) {
+      this.metaEntityService.findById(relationshipTarget).subscribe(metaEntity => this.targetMetaEntity = metaEntity);
+    }
 
     this.formGroup.valueChanges.subscribe((changes) => {
       const relationshipTarget = this.formGroup?.controls['relationshipTarget']?.value;
@@ -53,7 +56,18 @@ export class RelationshipTypeEditComponent implements OnInit {
   }
 
   toAttributeLabel(attributeName: string) {
-    return this?.targetMetaEntity.attributes.find(a => a.name === attributeName)?.label;
+    if(this.targetMetaEntity) {
+      const attribute = this.targetMetaEntity.attributes.find(a => a.name === attributeName);
+      if(attribute && attribute.label) {
+        return attribute.label;
+      }
+      else {
+        return '';
+      }
+    }
+    else {
+      return '';
+    }
   }
 
   removeSearchTerm(idx: number) {
