bc6dff5a801e980544a8c9497215fe8a6887747b
Richard Perfect <richard@june365.com>
2022-05-31 08:08:41 +1200

    Sequelize connection pooling

--- nestjs-server/src/orm/database.providers.ts
+++ nestjs-server/src/orm/database.providers.ts
@@ -100,20 +100,23 @@ export const databaseProviders = [
     provide: 'SEQUELIZE',
     inject: [ConfigService],
     useFactory: async (configService: ConfigService) => {
-      // if (globalSequelize) {
-      //   return globalSequelize;
-      // } else {
-      const databaseSettings: DatabaseSettings = {
-        databaseHost: configService.get<string>('DATABASE_HOST'),
-        databasePort: configService.get<number>('DATABASE_PORT'),
-        databaseUser: configService.get<string>('DATABASE_USER'),
-        passwordProperty: configService.get<string>('DATABASE_PASSWORD'),
-        passwordKey: configService.get<string>('DATABASE_PASSWORD_KEY'),
-        databaseName: configService.get<string>('DATABASE_NAME'),
-      };
+      console.log('SEQUELIZE factory: started');
+      if (globalSequelize) {
+        console.log('SEQUELIZE factory: detected globalSequelize, using that');
+        return globalSequelize;
+      } else {
+        console.log('SEQUELIZE factory: no globalSequelize, will loadOrm()');
+        const databaseSettings: DatabaseSettings = {
+          databaseHost: configService.get<string>('DATABASE_HOST'),
+          databasePort: configService.get<number>('DATABASE_PORT'),
+          databaseUser: configService.get<string>('DATABASE_USER'),
+          passwordProperty: configService.get<string>('DATABASE_PASSWORD'),
+          passwordKey: configService.get<string>('DATABASE_PASSWORD_KEY'),
+          databaseName: configService.get<string>('DATABASE_NAME'),
+        };
 
-      return await loadOrm(databaseSettings);
-      // }
+        return await loadOrm(databaseSettings);
+      }
     },
   },
 ];
