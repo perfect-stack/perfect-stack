7bc357332f3b8d2cb7e0d9329fda5e0432ca46e6
Richard Perfect <richard@june365.com>
2022-06-13 09:13:01 +1200

    Public api exports

--- angular-workspace/projects/ngx-perfect-stack/src/public-api.ts
+++ angular-workspace/projects/ngx-perfect-stack/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/ngx-perfect-stack.service';
 export * from './lib/ngx-perfect-stack.component';
 export * from './lib/ngx-perfect-stack.module';
 export * from './lib/ngx-perfect-stack-config';
+export * from './lib/data/data-service/data.service';
 export * from './lib/utils/toasts/toasts.component';
 export * from './lib/event/event.service';
 export * from './lib/event/page-listener';
