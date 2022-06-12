bd7a9a11645aec7a6cd0af523319c4f5efe82607
Richard Perfect <richard@june365.com>
2022-06-13 10:41:00 +1200

    Timezone integration issues with usage outside of library

--- angular-workspace/projects/ngx-perfect-stack/src/lib/data/controller/layout/controls/date-time-control/date-time-control.component.ts
+++ angular-workspace/projects/ngx-perfect-stack/src/lib/data/controller/layout/controls/date-time-control/date-time-control.component.ts
@@ -30,11 +30,15 @@ export class DateTimeControlComponent implements OnInit {
       const formControl = this.formGroup.controls[this.name];
       const databaseValue = formControl.value;
       if(databaseValue) {
+        console.log(`DateTimeControlComponent: databaseValue = ${databaseValue}`);
         let zonedDateTime = ZonedDateTime.parse(databaseValue);
+        console.log(`DateTimeControlComponent: zonedDateTime`, zonedDateTime );
         const zoneId = ZoneId.of('Pacific/Auckland');
+        console.log(`DateTimeControlComponent: zoneId`, zoneId );
         if(zoneId) {
           zonedDateTime = zonedDateTime.withZoneSameInstant(zoneId);
           this.dateTimeValue = DateTimeFormatter.ofPattern(this.stackConfig.dateTimeFormat).format(zonedDateTime);
+          console.log(`DateTimeControlComponent: dateTimeValue`, this.dateTimeValue );
         }
         else {
           console.error('Unable to load timezone - check that Timezone library has been imported');
