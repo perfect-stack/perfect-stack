ce2c03fe7ebc2be839090f3f815cdbdb9f26fc05
Richard Perfect <richard@june365.com>
2022-06-13 13:20:38 +1200

    Timezone integration issues with usage outside of library

--- angular-workspace/projects/ngx-perfect-stack/src/lib/data/controller/layout/controls/date-time-control/date-time-control.component.ts
+++ angular-workspace/projects/ngx-perfect-stack/src/lib/data/controller/layout/controls/date-time-control/date-time-control.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, Input, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
-import {DateTimeFormatter, ZonedDateTime, ZoneId} from '@js-joda/core';
+import {DateTimeFormatter, LocalDate, ZonedDateTime, ZoneId} from '@js-joda/core';
 import {NgxPerfectStackConfig, STACK_CONFIG} from '../../../../../ngx-perfect-stack-config';
 
 // This next import may appear to be grey and unused, but it is important because it imports the right language/words for
@@ -35,23 +35,14 @@ export class DateTimeControlComponent implements OnInit {
       const formControl = this.formGroup.controls[this.name];
       const databaseValue = formControl.value;
       if(databaseValue) {
-        console.log(`DateTimeControlComponent-1: databaseValue = ${databaseValue}`);
-        let zonedDateTime = ZonedDateTime.parse(databaseValue);
-        console.log(`DateTimeControlComponent-2: zonedDateTime`, zonedDateTime );
-        const zoneId = ZoneId.of('Pacific/Auckland');
-        console.log(`DateTimeControlComponent-3: zoneId`, zoneId );
-        if(zoneId) {
-          zonedDateTime = zonedDateTime.withZoneSameInstant(zoneId);
-          console.log(`DateTimeControlComponent-4: zonedDateTime`, zonedDateTime );
-          const dateTimeFormat = this.stackConfig.dateTimeFormat;
-          console.log(`DateTimeControlComponent-5: dateTimeFormat`, dateTimeFormat );
+        const dateTimeFormat = this.stackConfig.dateTimeFormat;
+        const dateTimeFormatter = DateTimeFormatter.ofPattern(dateTimeFormat).withLocale(Locale.US);
 
-          this.dateTimeValue = DateTimeFormatter.ofPattern(dateTimeFormat).format(zonedDateTime);
-          console.log(`DateTimeControlComponent-6: dateTimeValue`, this.dateTimeValue );
-        }
-        else {
-          console.error('Unable to load timezone - check that Timezone library has been imported');
-        }
+        let zonedDateTime = ZonedDateTime.parse(databaseValue);
+        const zoneId = ZoneId.of('Pacific/Auckland');
+        zonedDateTime = zonedDateTime.withZoneSameInstant(zoneId);
+
+        this.dateTimeValue = dateTimeFormatter.format(zonedDateTime);
       }
     }
   }
