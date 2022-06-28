5599e40a34335c213830583ecb86c2d4e21fdab6
Richard Perfect <richard@june365.com>
2022-06-29 09:03:28 +1200

    KIM-136 - Data goes missing in DB because relationship cols are dropped during model sync

--- nestjs-server/src/meta/meta-entity/meta-entity.service.ts
+++ nestjs-server/src/meta/meta-entity/meta-entity.service.ts
@@ -221,13 +221,6 @@ export class MetaEntityService {
       entityModelMap.set(nextMetaEntity.name, entityModel);
     }
 
-    // If alterDatabase then sync now so the tables are ready before we create their relationships
-    if (alterDatabase) {
-      for (const nextModel of entityModelMap.values()) {
-        await nextModel.sync({ alter: true });
-      }
-    }
-
     // second pass create the relationships
     for (const nextMetaEntity of databaseEntities) {
       for (const nextMetaAttribute of nextMetaEntity.attributes) {
@@ -249,6 +242,7 @@ export class MetaEntityService {
       }
     }
 
+    // Now that the models are fully defined ask Sequelize to sync the model schema with the database
     if (alterDatabase) {
       for (const nextModel of entityModelMap.values()) {
         await nextModel.sync({ alter: true });
