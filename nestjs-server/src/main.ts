f8b7f8eae401b2a27ed405e25acb8157c7ead3cf
Richard Perfect <richard@june365.com>
2022-11-09 11:09:30 +1300

    NestJS Upgrade and OpenAPI documentation

--- nestjs-server/src/main.ts
+++ nestjs-server/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Web API documentation')
+    .setDescription('The API documentation for the web services interface')
+    .setVersion('1.x')
+    .addTag('TAG')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3080);
 }
 bootstrap();
