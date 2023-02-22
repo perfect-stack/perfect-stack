34216905d87b6d6952d94a5ab501bf69b16ed5a9
Richard Perfect <richard@june365.com>
2023-02-23 09:17:30 +1300

    CallInstance bearing wrong data type

--- angular-workspace/projects/demo-client/src/app/pages/bird-view-page-listener.service.ts
+++ angular-workspace/projects/demo-client/src/app/pages/bird-view-page-listener.service.ts
@@ -6,6 +6,7 @@ import {MetaAttribute} from '../../../../ngx-perfect-stack/src/lib/domain/meta.e
 import {ParamMap, Router} from '@angular/router';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {AddEventDialogComponent} from './add-event-dialog/add-event-dialog.component';
+import {MetaPage, Template, TemplateLocationType} from '../../../../ngx-perfect-stack/src/lib/domain/meta.page';
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +43,43 @@ export class BirdViewPageListenerService implements PageListener {
   }
 
   onPageLoad(ctx: FormContext, params: ParamMap, queryParams: ParamMap): void {
+    const birdFormGroup = ctx.formMap.get('bird') as UntypedFormGroup;
+    if(birdFormGroup) {
+      const birdDataSource = birdFormGroup.controls['data_source'];
+      if (birdDataSource && birdDataSource.value === 'SkyRanger') {
+        this.hideBirdEditButton(ctx);
+      }
+    }
+  }
+
+  hideBirdEditButton(ctx: FormContext) {
+    const birdDetailsTemplate = this.recursiveTemplateSearch(ctx.metaPage, 'Bird details');
+    if(birdDetailsTemplate) {
+      console.log('FOUND bird details template');
+      const editButton = birdDetailsTemplate.locations[TemplateLocationType.BottomLeft] as any;
+      if(editButton) {
+        console.log('FOUND edit bird button');
+        editButton['actionPermit'] = 'NONE';
+      }
+    }
+  }
+
+  recursiveTemplateSearch(metaPage: MetaPage, templateHeading: string): Template | null {
+    for(const nextTemplate of metaPage.templates) {
+      if(nextTemplate.templateHeading === templateHeading) {
+        return nextTemplate;
+      }
+
+      for(const nextCellRow of nextTemplate.cells) {
+        for(const nextCell of nextCellRow) {
+          if(nextCell.template && nextCell.template.templateHeading === templateHeading) {
+            return nextCell.template;
+          }
+        }
+      }
+    }
+
+    return null;
   }
 
   onCompletion(ctx: FormContext): string {
