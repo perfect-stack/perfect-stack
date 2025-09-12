fdb0817929334facd598f50d5df02312b760066e
Richard Perfect <richard@perfect-consulting.co.nz>
2025-09-12 13:38:12 +1200

    MBD-359 Rfid file format - DB Snapshot

--- nestjs-server/libs/nestjs-server/src/orm/orm.service.ts
+++ nestjs-server/libs/nestjs-server/src/orm/orm.service.ts
@@ -1,15 +1,24 @@
-import { Inject } from '@nestjs/common';
-import { Sequelize } from 'sequelize';
-import { ConfigService } from '@nestjs/config';
-import { databaseProviders } from './database.providers';
+import {Inject, Logger} from '@nestjs/common';
+import {Sequelize} from 'sequelize';
+import {ConfigService} from '@nestjs/config';
+import {databaseProviders} from './database.providers';
 
 export class OrmService {
-  constructor(
-    @Inject('SEQUELIZE') public sequelize: Sequelize,
-    protected readonly configService: ConfigService,
-  ) {}
 
-  async reload() {
-    this.sequelize = await databaseProviders[0].useFactory(this.configService);
-  }
+    private logger = new Logger(OrmService.name);
+
+    constructor(
+        @Inject('SEQUELIZE') public sequelize: Sequelize,
+        protected readonly configService: ConfigService,
+    ) {
+    }
+
+    async reload() {
+        try {
+            this.sequelize = await databaseProviders[0].useFactory(this.configService);
+        } catch (e) {
+            this.logger.error(e.message, e.stack);
+            throw e;
+        }
+    }
 }
