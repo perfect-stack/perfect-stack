c38bf97f95d4145e2f0b6af2314acbabe493c258
Richard Perfect <richard@perfect-consulting.co.nz>
2026-09-19 18:55:26 +1200

    Remove AWS and CDK demo apps and associated configuration files

--- demo/aws/demo-aws-cdk/copy-client-json.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Copy the client.env file for the current environment into it's S3 location so that CloudFront can use it
-aws s3 cp $ENV_HOME/$ENV_NAME/client.json s3://demo-aws-client-release/env/$ENV_NAME/client.json
