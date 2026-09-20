1cc0fc02269762cdc2efa3dfdeef519f6564d6c2
Richard Perfect <richard@perfect-consulting.co.nz>
2026-09-20 18:56:52 +1200

    fix(ngx-perfect-stack): restore wildcard Any and case-insensitive permission matching in AuthorizationService

--- angular-workspace/projects/ngx-perfect-stack/src/lib/ngx-perfect-stack.component.spec.ts
+++ angular-workspace/projects/ngx-perfect-stack/src/lib/ngx-perfect-stack.component.spec.ts
@@ -1,14 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { NgxPerfectStackComponent } from "./ngx-perfect-stack.component";
+import { STACK_CONFIG, NgxPerfectStackConfig } from "./ngx-perfect-stack-config";
+import { AuthenticationService } from "./authentication/authentication.service";
+import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { BehaviorSubject, of } from "rxjs";
+import { ToastService } from "./utils/toasts/toast.service";
 
-import { NgxPerfectStackComponent } from './ngx-perfect-stack.component';
-
-describe('NgxPerfectStackComponent', () => {
+describe("NgxPerfectStackComponent", () => {
   let component: NgxPerfectStackComponent;
   let fixture: ComponentFixture<NgxPerfectStackComponent>;
 
+  const mockConfig: NgxPerfectStackConfig = {
+    apiUrl: "http://localhost:3080",
+    authenticationProvider: "None",
+  } as any;
+
+  const mockAuthService = {
+    user$: new BehaviorSubject(null),
+    notifyUser$: new BehaviorSubject(null),
+    isLoggedIn: false,
+  };
+
+  const mockToastService = {
+    show: () => {},
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NgxPerfectStackComponent ]
+      declarations: [ NgxPerfectStackComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        { provide: STACK_CONFIG, useValue: mockConfig },
+        { provide: AuthenticationService, useValue: mockAuthService },
+        { provide: ToastService, useValue: mockToastService },
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +48,7 @@ describe('NgxPerfectStackComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
